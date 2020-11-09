17e5914c358ad011235087e02b5f73d0f9bbdc96
AntonioEreiz332 <58472078+AntonioEreiz332@users.noreply.github.com>
2020-11-09 09:09:33 +0100

    Add files via upload

---
+++ AE-vjezba3/AE-vjezba3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AE_vjezba3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 4; double b = 10; double c = 4; double d = 5;
+            double x = (Math.Pow(a,2)+ Math.Pow(b, 2)+ Math.Pow(c, 2)+Math.Pow(d, 2))
+                / (Math.Pow(a+b, 2)+ Math.Pow(c+d, 2));
+            Console.WriteLine(x);
+            Console.ReadKey();
+        }
+    }
+}
